acd489610b305e25f0fe6043a18851569d43cd4e
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-20 14:24:26 -0300

    calculating the area of a triangle

--- figuras.js
+++ figuras.js
@@ -1,8 +1,33 @@
-const LadosDeUnCuadrado = 5;
-console.log("los lados del cuadrado miden:" + LadosDeUnCuadrado);
+//codigo de cuadrado, para agrupar usamos console.group()
+
+console.group("cuadrado");
+
+const LadosDeUnCuadrado = 15;
+console.log("los lados del cuadrado miden:" + LadosDeUnCuadrado + "cm");
 
 const PerimetroCuadrado = LadosDeUnCuadrado * 4;
-console.log("El perimetro del cuadrado es :" + PerimetroCuadrado );
+console.log("El perimetro del cuadrado es :" + PerimetroCuadrado + "cm" );
 
 const AreaCuadrado = LadosDeUnCuadrado*LadosDeUnCuadrado;
-console.log("El area de un cuadrado es :" + AreaCuadrado)
+console.log("El area de un cuadrado es :" + AreaCuadrado + "cm^2")
+
+console.groupEnd("cuadrado");
+
+
+console.group(triangulo);
+
+const ladoTriangulo1 = 5;
+const ladoTriangulo2 = 5;
+const BaseTriangulo = 3;
+
+console.log(
+    "Los lados del triangulo miden:"
+    +  ladoTriangulo1
+    + "cm, "
+    + ladoTriangulo2
+    + "cm, "
+    + BaseTriangulo
+    + "cm."
+);
+
+console.groupEnd("cuadrado");
