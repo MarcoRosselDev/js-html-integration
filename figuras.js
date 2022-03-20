cd4d1d48f531557e9a27b42cc27cf2cfb3ff8ff8
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-20 13:36:58 -0300

    calculating areas and perimeters of a square

---
+++ figuras.js
@@ -0,0 +1,8 @@
+const LadosDeUnCuadrado = 5;
+console.log("los lados del cuadrado miden:" + LadosDeUnCuadrado);
+
+const PerimetroCuadrado = LadosDeUnCuadrado * 4;
+console.log("El perimetro del cuadrado es :" + PerimetroCuadrado );
+
+const AreaCuadrado = LadosDeUnCuadrado*LadosDeUnCuadrado;
+console.log("El area de un cuadrado es :" + AreaCuadrado)
