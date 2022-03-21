7a13629d1cd4e704517babdfe4b76fba2ef38c70
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-21 08:23:44 -0300

    looking for solutions for interaction with html

--- figuras2.js
+++ figuras2.js
@@ -9,23 +9,24 @@ function areaCuadrado (lado) {
     return lado*lado;
 }
 
-console.groupEnd("cuadrado");
+console.groupEnd();
 
 
 console.group("triangulo");
 
 function perimetroTriangulo(lado1, lado2, base){
-    return lado1 + lado2 +base;
+    return lado1 + lado2 + base;
 }
 
 function areaTriangulo(base, altura){
-    return (base*altura)/2;
+    return (base*altura) / 2;
 }
 
-console.groupEnd("triangulo");
+console.groupEnd();
 
-console.group("circulo")
+console.group("circulo");
 
+function perimetroCirculo(radio);
     return radio*2*3.1415;
 }
 
@@ -33,24 +34,22 @@ function areaCirculo (radio){
     return radio*radio*3.1415;
 }
 
-
-
-console.groupEnd() 
+console.groupEnd();
 
 // interaccion con html
 
-function calcularPerimetroCuadrado(){
+function calcularPerimetroCuadrado() {
     const input = document.getElementById("inputCuadrado");
     const value = input.value;
 
-    const perimetro = perimetroCuadrado(value);
-    alert(perimetro);
+    const importante = perimetroCuadrado(value);
+    alert("el perimetro del cuadrado es:" + importante);
 }
 
-function calcularÁreaCuadrado(){
+function calcularÁreaCuadrado() {
     const input = document.getElementById("inputCuadrado");
     const value = input.value;
     
     const area = areaCuadrado(value);
-    alert(area);
+    alert("el area del cuadrado es :" + area);
 }
