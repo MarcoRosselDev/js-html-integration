ecdb1ba2be0ab254690b883f0ef28f25806beeaf
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-22 16:20:53 -0300

    applying the discounts and prices in functions

--- descuentos.js
+++ descuentos.js
@@ -1,12 +1,16 @@
-const precioOriginal = 120;
-const descuento = 18;
+// const precioOriginal = 120;
+// const descuento = 18;
 
-const porcentajePrecioConDescuento = 100 - descuento;
-const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+function clacularPecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
-console.log({
-    precioOriginal,
-    descuento,
-    porcentajePrecioConDescuento,
-    precioConDescuento,
-});
+    return precioConDescuento;
+}
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento,
+// });
