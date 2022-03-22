2a7caebf3390d64d23a2c6a27e8315210493348a
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-22 17:06:09 -0300

    interaction problems to solve

--- descuentos.js
+++ descuentos.js
@@ -8,6 +8,20 @@ function clacularPecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
+function interactionButton() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "el precio con descuento son : $" + precioConDescuento;
+}
+
 // console.log({
 //     precioOriginal,
 //     descuento,
