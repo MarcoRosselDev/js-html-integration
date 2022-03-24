7436bb787aa6217957c0946d2f1021eed22d12d0
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-24 20:08:10 -0300

    calculating the arithmetic mean with the reduct method of the arrays

--- promedio.js
+++ promedio.js
@@ -13,10 +13,18 @@ for (let i = 0; i < lista1.length; i++){
 const promedioLista1 = sumaLista1 / lista1.length;
 
 function calcularMediaAritmetica(lista) {
-    let sumaLista = 0;
-    for (let i = 0; i < lista.length; i++) {
-        sumaLista = sumaLista + lista[i];
-    }
+    // let sumaLista = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    // podemos trabajar con este otro metodo
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
 
     const promedioLista = sumaLista / lista.length;
     return promedioLista;
