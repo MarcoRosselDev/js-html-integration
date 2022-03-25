519303e90d4554ff03f6b9bdd4fab0db028133f0
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-25 13:08:35 -0300

    javascript and html interaction documentation notes

---
+++ interacciónes.js
@@ -0,0 +1,5 @@
+ramdomName = "hola, buen día.";
+
+interacciónConHtml = "1-" + ramdomName + "prueba de la interacción de js con html usando document.weite(x var) ."
+
+document.write(interacciónConHtml) // podemos incorporar el resultado de un algoritmo usando este llamado incrustandolo en html.
