e5eddb12766d356c9514c5b4c5813b274679c05c
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-25 23:37:55 -0300

    break for reed documentation

--- interacciónes.js
+++ interacciónes.js
@@ -3,3 +3,12 @@ ramdomName = "hola, buen día.";
 interacciónConHtml = "1-" + ramdomName + "prueba de la interacción de js con html usando document.weite(x var) ."
 
 document.write(interacciónConHtml) // podemos incorporar el resultado de un algoritmo usando este llamado incrustandolo en html.
+
+
+
+// recomendacion del uso de back tips ( ` ).
+
+nombre = "Marco Rossel";
+frase = `soy ${nombre} y estoy aprendiendo javascript.`;//ayuda a evitat un monton de errores
+document.write(frase);
+
