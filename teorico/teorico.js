3ec868972157d0c27be387645896900e460ff8f7
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-28 16:45:51 -0300

    request information and weite documents from javascript

--- teorico/teorico.js
+++ teorico/teorico.js
@@ -1,3 +1,9 @@
 // imprimir un mensaje al usuario en forma de alerta ¡ 
     let numero = "1-";
     alert(numero + "hola");
+
+// solicitar información al usuario.
+
+    let nombre = prompt("dime tu nombre");
+
+    document.write("hola " + nombre);
