1d630078c0c818da3c9dcff4bf9e3a0be9355759
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-28 22:47:11 -0300

    changes the structure

--- teorico/teorico.js
+++ teorico/teorico.js
@@ -1,4 +1,12 @@
 // imprimir un mensaje al usuario en forma de alerta ¡
 
-let nombre = prompt("dime tu nombre");
-document.write("hola " + nombre + ",  buen dia.");
+// let nombre = prompt("dime tu nombre");
+// document.write("hola " + nombre + ",  buen dia.");
+
+
+// probando los operadores de asignación 
+
+let numero1 = 3;
+let numero2 = 8;
+
+document.write(numero2 ** numero1)
