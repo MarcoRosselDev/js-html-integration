281063fc89b9b7acc5faa372b8de0c1ba59fdada
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-29 16:58:38 -0300

    good practices in the declaration of variables

--- teorico/teorico.js
+++ teorico/teorico.js
@@ -1,7 +1,7 @@
 // imprimir un mensaje al usuario en forma de alerta ¡
 
-// let nombre = prompt("dime tu nombre");
-// document.write("hola " + nombre + ",  buen dia.");
+let nombre = prompt("dime tu nombre");
+document.write("hola " + nombre + ",  buen dia.");
 
 
 // probando los operadores de asignación 
@@ -9,4 +9,22 @@
 let numero1 = 3;
 let numero2 = 8;
 
-document.write(numero2 ** numero1)
+document.write(numero2 ** numero1 );
+
+//probando la concatenación 
+
+let saludo = "hola ¡";
+let pregunta = "¿como estas?";
+let frase = saludo + pregunta;
+
+document.write(frase);
+
+// probando la ${ } para llamar contenido de letss
+
+let prgunta2 = "¿como estas " + nombre +"?";
+document.write(prgunta2);
+
+// mejorado usando ``
+
+let pregunta3 = `¿como estas mi amigo ${nombre}?`;
+document.write(pregunta3);
