f94e1d25201792c118157ea05e1e66d369263917
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-29 17:21:18 -0300

    testing line break in phrases using variables

--- teorico/teorico.js
+++ teorico/teorico.js
@@ -1,7 +1,7 @@
 // imprimir un mensaje al usuario en forma de alerta ¡
 
 let nombre = prompt("dime tu nombre");
-document.write("hola " + nombre + ",  buen dia.");
+document.write("hola " + nombre + ",  buen dia.<br>");
 
 
 // probando los operadores de asignación 
@@ -13,7 +13,7 @@ document.write(numero2 ** numero1 );
 
 //probando la concatenación 
 
-let saludo = "hola ¡";
+let saludo = "<br> hola ¡";
 let pregunta = "¿como estas?";
 let frase = saludo + pregunta;
 
@@ -21,10 +21,10 @@ document.write(frase);
 
 // probando la ${ } para llamar contenido de letss
 
-let prgunta2 = "¿como estas " + nombre +"?";
+let prgunta2 = "<br> ¿como estas " + nombre +"?";
 document.write(prgunta2);
 
 // mejorado usando ``
 
-let pregunta3 = `¿como estas mi amigo ${nombre}?`;
+let pregunta3 = `<br>¿como estas mi amigo ${nombre}?`;
 document.write(pregunta3);
