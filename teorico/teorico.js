56f539bd7bb026aef852c5e3cf55454d6cda03ae
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-29 22:46:17 -0300

    comparadores logicos

--- teorico/teorico.js
+++ teorico/teorico.js
@@ -26,5 +26,20 @@ document.write(prgunta2);
 
 // mejorado usando ``
 
-let pregunta3 = `<br>¿como estas mi amigo ${nombre}?`;
+let pregunta3 = `<br>¿como estas mi amigo ${nombre}? <br>`;
 document.write(pregunta3);
+
+//comparadores logicos.
+
+// equality        (a == b)    igualdad
+// inequality      (a != b)    desigualdad
+// identity        (a === b)   identico
+// non-identity    (a !== b)   no-identico
+// greater than    (a > b)     mayor que 
+// greater than or equality    (a >= b)    mayor o igual
+// less than       (a < b)     menor que  
+// less than or equal          (a <= b)    menor o igual 
+
+//ejemplo
+
+document.write(numero1 !== numero2);
