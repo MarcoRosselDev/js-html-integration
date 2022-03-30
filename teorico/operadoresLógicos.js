ff427455012321f62e3a6927970afeef1b836e7d
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-30 20:56:21 -0300

    testing multilple selectors and changes operators.

--- teorico/operadoresLógicos.js
+++ teorico/operadoresLógicos.js
@@ -10,11 +10,11 @@ let afirmación = true;
 let afirmación2 = true;
 
 let resultadoAfirmación = afirmación &&  afirmación2;     // solo si las 2 son true devuelve true 
-let resultadoAfirmación2 = afirmación || afirmación2;     // basta con un true devuelve true
 let resultadoAfirmación3 = afirmación =! afirmación2;     // 
+let resultadoAfirmación2 = afirmación || afirmación2;     // basta con un true devuelve true
 
-document.write(resultadoAfirmación);
 document.write(resultadoAfirmación2);
 document.write(resultadoAfirmación3);
+document.write(resultadoAfirmación);
 
 // para escribir en varias partes a la vez precionar alt + click en donde queremos escribir.
