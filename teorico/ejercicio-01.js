4e60b6ecc3a89edc65cbd8b5da6bbc0d4fba6433
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-30 23:43:06 -0300

    exercise 1 firt steps

---
+++ teorico/ejercicio-01.js
@@ -0,0 +1,11 @@
+// tengo que saber cuanto dinero tienen 3 amigos.
+// quieren comprar los hela dos más caros posibles.
+// quieren saber el vuelto. 
+
+let juan = prompt(`¿cuanto dinero tienes juan ?`);
+let luis = prompt(`¿cuanto dinero tienes  luis?`);
+let marco = prompt(`¿cuanto dinero tienes  marco?`);
+
+document.write(`juan tiene; $`  + juan  + `<br>`);
+document.write(`luis tiene; $` + luis  + `<br>`);
+document.write(`marco tiene; $` + marco  + `<br>`);
