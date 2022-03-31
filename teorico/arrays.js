58e9e0ff4487902bf381ea7367f9a1d38084261a
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-31 03:04:07 -0300

    arrays documentation notes

--- teorico/arrays.js
+++ teorico/arrays.js
@@ -6,3 +6,15 @@ document.write(frutas);
 // document.write(frutas[ y el numero a acceder ])
 
 document.write(frutas[3]);
+
+// arrays asociativos
+
+let pc = {
+    nombre : "Marcoss pc",
+    marca : "asus",
+    annio : "2020",
+    ram : "8GB"
+}
+
+document.write(pc["marca"]);     //error puse pc(marca)
+                                 //dever llevar " " .
