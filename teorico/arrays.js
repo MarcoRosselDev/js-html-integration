7be3356c723edd854859ffa5605e0adaf3ed7282
Marco Rossel <andresmarcorossel@gmail.com>
2022-03-31 02:49:26 -0300

    declation and sintaxis of arrays

---
+++ teorico/arrays.js
@@ -0,0 +1,8 @@
+frutas = ["pera","manzana","banana"];
+
+document.write(frutas);
+
+//para acceder a x elemento de un array usamos por ejemplo
+// document.write(frutas[ y el numero a acceder ])
+
+document.write(frutas[3]);
