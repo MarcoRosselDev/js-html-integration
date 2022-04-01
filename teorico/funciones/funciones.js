d5573792c8f99d976f74af95ecebd40e0db02005
Marco Rossel <andresmarcorossel@gmail.com>
2022-04-01 01:29:35 -0300

    how to create functions

---
+++ teorico/funciones/funciones.js
@@ -0,0 +1,21 @@
+//notas de documentación de funciones en javascript
+
+//temas
+
+//1-definición y usos 
+//2-forma de crear funciones
+//3-return
+//4-parametros
+//5-scope
+//6-funciones flecha
+
+//1-definición y usos 
+
+//Una función en JavaScript es similar a un procedimiento — un conjunto de instrucciones que realiza una tarea o calcula un valor, pero para que un procedimiento califique como función, debe tomar alguna entrada y devolver una salida donde hay alguna relación obvia entre la entrada y la salida.
+
+//2-forma de crear funciones
+
+function saludar()  {                   //podemos llamar despues con saludar()
+    prompt(`hola  ¿como estas?`);
+}
+saludar();
